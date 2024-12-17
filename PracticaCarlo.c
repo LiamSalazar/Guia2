2d26d8e8f35919e9c336f1ea6a20148ad06aaa2f
LiamSalazar <150866232+LiamSalazar@users.noreply.github.com>
2024-12-17 12:58:13 -0600

    Update PracticaCarlo.c

--- PracticaCarlo.c
+++ PracticaCarlo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 void mostrarMenu();
 void registrarValores(int *calificaciones);
 float promedio_calificaciones();
@@ -14,11 +15,17 @@ int main(){
 
 void mostrarMenu(){
     int opcion;
-    int calificaciones [10];
+    int calificaciones[10];
     p = calificaciones;
-    do{
-        printf("Favor de seleccionar una opcion:");
+    do {
+        printf("1) Registrar valores\n");
+        printf("2) Promedio de calificaciones\n");
+        printf("3) Calificacion mas baja\n");
+        printf("4) Moda de calificaciones\n");
+        printf("5) Salir\n");
+        printf("Favor de seleccionar una opcion: ");
         scanf("%d", &opcion);
+
         if(opcion == 1){
             registrarValores(calificaciones);
         }
@@ -30,34 +37,35 @@ void mostrarMenu(){
             int resultado2 = calificacionBaja(calificaciones);
             printf("La calificacion mas baja es: %d\n", resultado2);
         }
-        }while (opcion!= 5);
+        if(opcion == 4){
+            modaCalificaciones(calificaciones);
+        }
+    } while (opcion != 5);
 
-    }
+    printf("Gracias por usar el programa. ¡Adios!\n");
+}
+
 void registrarValores(int *calificaciones){
     int *cal = calificaciones;
-    int *liam;
-    liam = calificaciones;
     for (int i = 0; i < 10; i++){
+        printf("Ingrese la calificacion %d: ", i+1);
         scanf("%d", cal);
         cal++;
     }
-    for (int j = 0; j< 10; j++){
-        printf("%d ", *liam);
-        liam++;
-    }
-    printf("\n");
+    printf("Calificaciones registradas correctamente.\n");
 }
+
 float promedio_calificaciones(){
-    int *j;
-    j = p;
+    int *j = p;
     int suma = 0;
     for(int i = 0; i < 10; i++){
-    suma+= *j;
-    j++;
+        suma += *j;
+        j++;
     }
-    int promedio =suma/10;
+    float promedio = suma / 10.0; 
     return promedio;
 }
+
 int calificacionBaja(int *calificaciones){
     int mincalificacion = 100;
     int *ptr = calificaciones; 
@@ -68,4 +76,27 @@ int calificacionBaja(int *calificaciones){
         ptr++; 
     }
     return mincalificacion;
+}
+
+void modaCalificaciones(int *calificaciones){
+    int *calif = calificaciones;
+    int frecuencia[101] = {0}; 
+    int moda = -1, maxFrecuencia = 0;
+
+    for (int i = 0; i < 10; i++){
+        frecuencia[calif[i]]++;
+    }
+
+    for (int i = 0; i <= 100; i++){
+        if (frecuencia[i] > maxFrecuencia){
+            maxFrecuencia = frecuencia[i];
+            moda = i;
+        }
+    }
+
+    if (maxFrecuencia > 1){
+        printf("La moda es: %d (se repite %d veces)\n", moda, maxFrecuencia);
+    } else {
+        printf("No hay moda, todos los valores son únicos.\n");
+    }
 }
